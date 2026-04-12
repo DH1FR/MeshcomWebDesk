7ee4aa9127482770a3ce53a621c9026bd5501759
Ralf Altenbrand <RAltenbrand@altenbrand.de>
2026-04-12 13:00:27 +0200

    fix(map): include direct broadcasts in DirectLinkConfirmed

--- MeshcomWebDesk/Services/ChatService.cs
+++ MeshcomWebDesk/Services/ChatService.cs
@@ -431,7 +431,7 @@ public class ChatService
                 LastRelayPath        = message.RelayPath,
                 HopCount             = message.RelayPath?.Split(',').Length - 1 ?? 0,
                 RelayPathCount       = message.RelayPath != null ? 1 : 0,
-                DirectLinkConfirmed  = (message.IsAck || (!message.IsPositionBeacon && !message.IsTelemetry && !message.IsBroadcast))
+                DirectLinkConfirmed  = (message.IsAck || (!message.IsPositionBeacon && !message.IsTelemetry))
                                        && message.RelayPath == null,
                 Temp1             = message.IsTelemetry ? message.Temp1     : null,
                 Humidity          = message.IsTelemetry ? message.Humidity  : null,
@@ -452,7 +452,7 @@ public class ChatService
                 if (message.Battery.HasValue) s.Battery  = message.Battery;
                 if (message.HwId.HasValue)    s.HwId     = message.HwId;
                 if (!string.IsNullOrEmpty(message.Firmware)) s.Firmware = message.Firmware;
-                if ((message.IsAck || (!message.IsPositionBeacon && !message.IsTelemetry && !message.IsBroadcast))
+                if ((message.IsAck || (!message.IsPositionBeacon && !message.IsTelemetry))
                     && message.RelayPath == null)
                     s.DirectLinkConfirmed = true;
 
